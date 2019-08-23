ec9e4dad773ebc13f51abacb47404b2b47700941
Nikola Milikic <nikola.milikic@gmail.com>
2019-08-23 14:00:15 +0200

    Sredjen problem sa kreiranjem bigrama i trigrama. Mala izmena u redosledu koraka preprocesiranja.

--- script.R
+++ script.R
@@ -1,6 +1,6 @@
 #install.packages("tm")
-#install.packages("tidyr") - koriscena fja unite
-#install.packages("qdap") - koriscena fja za najfrekventnije termine
+#install.packages("tidyr") #koriscena fja unite
+#install.packages("qdap") #koriscena fja za najfrekventnije termine
 #install.packages("RWeka")
 library(tm)
 library(tidyr)
@@ -26,6 +26,7 @@ length(which(ecommerce_reviews$Title=="" & ecommerce_reviews$Review.Text==""))
 
 #posto ovakvih unosa, gde nemamo ni naslov ni tekst komentara ima 3% (=844/23486) od ukupnog dataseta,
 #da li zanemariti ovo i izbaciti te unose?
+# Nikola: izbaciti, nije moguce zameniti ih necim. Ali neka ostane ovaj korak u skripti i spomenucete ga i u tekstu
 
 na.indeksi <- which(ecommerce_reviews$Title=="" & ecommerce_reviews$Review.Text=="")
 ecommerce_reviews <- ecommerce_reviews[-na.indeksi,] #ovim se smanjuje dataset na 22642 opservacije
@@ -47,6 +48,7 @@ ecommerce_reviews <- unite(ecommerce_reviews, "Review", c("Title","Review.Text")
 #(Rating i Recommended.IND)
 corr.matrix <- cor(ecommerce_reviews[-1])
 corrplot(corr.matrix, method = "number", type = "upper", diag = F, number.cex = 3.5, tl.cex = 1, tl.srt=0,  tl.col="black")
+# Nikola: nema potrebe ovo crtati, racunate korelaciju izmedju dve varijable samo 
 
 #sa grafika vidimo da je koef. korelacije 0.79, sto se smatra jakom korelacijom
 #(kako raste Rating (ka oceni 5), tako Recommended.IND naginje vise ka 1 (dakle da osoba preporucuje proizvod))
@@ -54,13 +56,16 @@ corrplot(corr.matrix, method = "number", type = "upper", diag = F, number.cex = 
 
 #Oznacavanje podataka: ocene 1 i 2 -> NEG, ocene 4 i 5 -> POS,
 #ali moramo videti sta cemo raditi sa ocenom 3
-prop.table(table(ecommerce_reviews$Rating,ecommerce_reviews$Recommended.IND))
+prop.table(table(ecommerce_reviews$Rating,ecommerce_reviews$Recommended.IND), margin = 1)
+# Nikola: dodao sam margin = 1 kako bi se redovi sumirali u 100%, odnosno u 1
 
 #Na osnovu raspodele vrednosti Rating po tome da li su preporucene (Recommended.IND=1) ili ne,
 #vidimo da je priblizno jednaka raspodela ocene 3 (7% ne preporucuje, dok 5% preporucuje).
 #Posto ni jedna "strana" raspodele ne dominira, zakljucak :
 #ocena 3 se moze iskljuciti iz daljeg razmatranja za oznacavanje podataka.
 ecommerce_reviews <- ecommerce_reviews[-which(ecommerce_reviews$Rating==3),]
+
+summary(ecommerce_reviews)
 
 #Oznacavamo preostale opservacije.
 ecommerce_reviews$Label <- ifelse(test = ecommerce_reviews$Rating == 1 | ecommerce_reviews$Rating == 2,
@@ -74,19 +79,21 @@ prop.table(table(ecommerce_reviews$Label))
 #11.9% negativnih komentara i 88% pozitivnih, dakle neravnomerna distribucija komentara u korist pozitivnih
 
 #Mozda dodati ovu varijablu. Mada po grafiku, nema neke predvidive zakonitosti.
-#ecommerce_reviews$ReviewLength <- nchar(ecommerce_reviews$Review)
-#summary(ecommerce_reviews$ReviewLength)
-#ggplot(ecommerce_reviews, aes(x = ReviewLength, fill = Label)) +
-#theme_bw() +
-#  geom_histogram(binwidth = 5) +
-#  labs(y = "Review Count", x = "Length of Review",
-#       title = "Distribution of Review Lengths with Class Labels")
+# Nikola: mislim da nije korisna ova varijabla
+ecommerce_reviews$ReviewLength <- nchar(ecommerce_reviews$Review)
+summary(ecommerce_reviews$ReviewLength)
+ggplot(ecommerce_reviews, aes(x = ReviewLength, fill = Label)) + 
+  theme_bw() +
+  geom_histogram(binwidth = 5) +
+  labs(y = "Review Count", x = "Length of Review",
+       title = "Distribution of Review Lengths with Class Labels")
 
 #Sredjivanje naziva redova, tako da idu redom
+# Nikola: ne shvatam potrebu za ovim korakom
 row.names(ecommerce_reviews) <- seq(1:nrow(ecommerce_reviews))
 
 #Kreiranje korpusa
-corpus_review <- Corpus(VectorSource(ecommerce_reviews$Review))
+corpus_review <- VCorpus(VectorSource(ecommerce_reviews$Review))
 #Proba sa VCorpus
 #corpus_review <- VCorpus(VectorSource(ecommerce_reviews))
 inspect(corpus_review)
@@ -115,18 +122,20 @@ plot(freq.terms)
 #Uklonicemo top3 reci: dress, love i size, jer njihova frekventnost ne utice na prediktivnost sentimenta
 corpus_review <- tm_map(corpus_review, removeWords, c("pos","dress","love","size"))
 
+#Uklanjanje brojeva
+corpus_review <- tm_map(corpus_review, removeNumbers)
+
 #Uklanjanje znakova interpunkcije
 corpus_review <- tm_map(corpus_review , removePunctuation,
                         preserve_intra_word_contractions = TRUE,
                         preserve_intra_word_dashes = TRUE)
 
-#Uklanjanje brojeva
-corpus_review <- tm_map(corpus_review, removeNumbers)
-
 #Uklanjanje nepotrebnih razmaka
 corpus_review <- tm_map(corpus_review, stripWhitespace)
 
 #Stemovanje reci
+#install.packages('SnowballC')
+library(SnowballC)
 corpus_review <- tm_map(corpus_review , stemDocument, language = "english")
 
 
@@ -136,6 +145,9 @@ corpus_review <- tm_map(corpus_review , stemDocument, language = "english")
 #Kreiranje DTM matrice (po defaultu su ovo unigrami)
 dtm <- DocumentTermMatrix(corpus_review)
 inspect(dtm)
+
+dtm.trimmed <- removeSparseTerms(dtm, sparse = 0.9875)
+inspect(dtm.trimmed)
 
 #Racunamo frekvencije termina iz DTM
 freq <- colSums(as.matrix(dtm))
@@ -155,6 +167,8 @@ freq[tail(ord)]
 #incomplete.cases <- which(!complete.cases(as.matrix(dtm)))
 #incomplete.cases
 
+inspect(corpus_review)
+
 #Kreiramo TF-IDF matricu
 dtm.tfidf <- DocumentTermMatrix(corpus_review, control = list(weighting = weightTfIdf))
 #dtm.tfidf <- removeSparseTerms(dtm.tfidf, 0.9999)
@@ -163,11 +177,12 @@ dtm.tfidf <- dtm.tfidf[rowTotals > 0, ]
 inspect(dtm.tfidf)
 
 #Kreiranje bigrama
-BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+BigramTokenizer <- function(x) unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
 dtm.bigram <- DocumentTermMatrix(corpus_review, control = list(tokenize = BigramTokenizer))
 inspect(dtm.bigram)
 
 #Kreiranje trigrama
-TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+TrigramTokenizer <- function(x) { unlist(lapply(ngrams(words(x), 3), paste, collapse = " "), use.names = FALSE)}
 dtm.trigram <- DocumentTermMatrix(corpus_review, control = list(tokenize = TrigramTokenizer))
 inspect(dtm.trigram)
+
